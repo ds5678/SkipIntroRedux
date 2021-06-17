8a2cc68b85bbf9ca9879fc2dd7cb9381f1b06969
ds5678 <jrpritts7@gmail.com>
2021-06-17 12:39:46 -0400

    Hide newsletter button

---
+++ src/BuildInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SkipIntroRedux
+{
+    public static class BuildInfo
+    {
+        public const string Name = "SkipIntroRedux"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Skips to the main menu."; // Description for the Mod.  (Set as null if none)
+        public const string Author = "AlexTheRegent, ds5678"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "2.2.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+}
---
+++ src/Implementation.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace SkipIntroRedux
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+        }
+    }
+}
