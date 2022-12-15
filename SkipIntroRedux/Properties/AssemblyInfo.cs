a4cf5631e288a7dab5b09ba626ba1a944fcee186
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-14 23:37:35 -0500

    Clean up the assembly info

--- SkipIntroRedux/Properties/AssemblyInfo.cs
+++ SkipIntroRedux/Properties/AssemblyInfo.cs
@@ -3,18 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using BuildInfo = SkipIntroRedux.BuildInfo;
 
-[assembly: ComVisible(false)]
-[assembly: Guid("4a02cc24-0378-4b54-a06f-1adfaa7ff1e5")]
-
 [assembly: AssemblyTitle(BuildInfo.Name)]
-[assembly: AssemblyDescription(BuildInfo.Description)]
-[assembly: AssemblyCompany(BuildInfo.Company)]
 [assembly: AssemblyProduct(BuildInfo.Name)]
 [assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
-[assembly: AssemblyTrademark(BuildInfo.Company)]
-[assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion(BuildInfo.Version)]
 [assembly: AssemblyFileVersion(BuildInfo.Version)]
-[assembly: MelonInfo(typeof(SkipIntroRedux.Implementation), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
+[assembly: MelonInfo(typeof(SkipIntroRedux.Implementation), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
